a22b48969a581acae4376c91869aa6d5ef23061a
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-06-01 18:13:54 -0300

    criada rotas de put e delete

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -13,14 +13,33 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/new', async (req, res) => {
+    try {
+        let checklist = new Checklist()
+        res.status(200).render('checklists/new', {checklist: checklist})
+    } catch (error) {
+        res.status(500).render('pages.error', {error: 'Erro ao carregar o formulário'})
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).render('checklists/edit', { checklist: checklist })
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Erro ao exibir a area de edição' })
+    }
+})
+
 router.post('/', async (req, res) => {
-    let { name } = req.body
+    let { name } = req.body.checklist
+    let checklist = new Checklist({name})
     
     try {
-        let checklist = await Checklist.create({ name })
-        res.status(200).json(checklist)
+        await checklist.save()
+        res.redirect('/checklists')
     } catch (error) {
-        res.status(422).json(error)
+        res.status(422).render('checklists/new', { checklists: { ...checklist, error } })
     }
 })
 
@@ -29,27 +48,29 @@ router.get('/:id', async (req, res) => {
         let checklist = await Checklist.findById(req.params.id)
         res.status(200).render('checklists/show', { checklist: checklist })
     } catch (error) {
-        res.status(422).render('pages.error', { error: 'Erro ao exibir as Listas de tarefas' })
+        res.status(500).render('pages/error', { error: 'Erro ao exibir as Listas de tarefas' })
     }
 })
 
 router.put('/:id', async (req, res) => {
-    let { name } = req.body
+    let { name } = req.body.checklist
+    let checklist = await Checklist.findById(req.params.id)
 
     try {
-        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true})
-        res.status(200).json(checklist)
+        await checklist.update({name})
+        res.redirect('/checklists')
     } catch (error) {
-        res.status(422).json(error)
+        let errors = error.errors
+        res.status(422).render('checklists/edit', { checklist: {...checklist, errors} })
     }
 })
 
 router.delete('/:id', async (req, res) => {
     try {
-        let checklist = await Checklist.findByIdAndRemove(req.params.id)
-        res.status(200).json(checklist)
+        await Checklist.findByIdAndRemove(req.params.id)
+        res.redirect('/checklists')
     } catch (error) {
-        res.status(422).json(error)
+        res.status(500).render('pages/error', { error: 'Erro ao excluir checklist' })
     }
 })
 
